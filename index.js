73cb185d90ba49bacfbd24375b9d53f9ed0c6ab4
ForesterMW <39112043+tuikat@users.noreply.github.com>
2025-02-04 11:26:31 +1300

    Removed /add-server /add-node endpoints
    will be added back in later

--- index.js
+++ index.js
@@ -28,14 +28,6 @@ class MultiServerMetricsConfig {
     }
   }
 
-  saveConfig() {
-    fs.writeFileSync(
-      this.configPath, 
-      JSON.stringify(this.config, null, 2), 
-      'utf8'
-    );
-  }
-
   setupGauges() {
     // Single fan speed gauge for all servers
     this.fanSpeedGauge = new client.Gauge({
@@ -46,56 +38,10 @@ class MultiServerMetricsConfig {
     this.registry.registerMetric(this.fanSpeedGauge);
   }
 
-  addServer(name, ip, port) {
-    this.config.servers[name] = {
-      ip,
-      port,
-      nodes: {
-        fans: []
-      }
-    };
-    this.saveConfig();
-    return this.config.servers[name];
-  }
-
   generateLocation(path) {
     // Extract the last part of the path as location
     const pathParts = path.split('.');
     return pathParts[pathParts.length - 1];
-  }
-
-  addNode(serverName, nodeType, nodeDetails) {
-    if (!this.config.servers[serverName]) {
-      throw new Error(`Server ${serverName} not found`);
-    }
-    
-    // Ensure the node type exists in the configuration
-    if (!this.config.servers[serverName].nodes[nodeType]) {
-      this.config.servers[serverName].nodes[nodeType] = [];
-    }
-
-    // Generate location from path if not provided
-    const finalNodeDetails = {
-      ...nodeDetails,
-      location: nodeDetails.location || this.generateLocation(nodeDetails.path)
-    };
-
-    // Add the new node
-    this.config.servers[serverName].nodes[nodeType].push(finalNodeDetails);
-    this.saveConfig();
-    return finalNodeDetails;
-  }
-
-  addFan(serverName, path, name) {
-    // Generate location from path, use default name if not provided
-    const location = this.generateLocation(path);
-    const finalName = name || `fan_${location}`;
-
-    return this.addNode(serverName, 'fans', { 
-      path, 
-      name: finalName, 
-      location 
-    });
   }
 }
 
@@ -166,28 +112,6 @@ const nodeMetricsConfig = new MultiServerMetricsConfig(configPath);
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', nodeMetricsConfig.registry.contentType);
   res.end(await nodeMetricsConfig.registry.metrics());
-});
-
-app.post('/add-server', express.json(), (req, res) => {
-  const { name, ip, port } = req.body;
-  const newServer = nodeMetricsConfig.addServer(name, ip, port);
-  res.json(newServer);
-});
-
-app.post('/add-node', express.json(), (req, res) => {
-  const { serverName, nodeType, nodeDetails } = req.body;
-  try {
-    const newNode = nodeMetricsConfig.addNode(serverName, nodeType, nodeDetails);
-    res.json(newNode);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-app.post('/add-fan', express.json(), (req, res) => {
-  const { serverName, path, name } = req.body;
-  const newFan = nodeMetricsConfig.addFan(serverName, path, name);
-  res.json(newFan);
 });
 
 app.listen(port, () => {
